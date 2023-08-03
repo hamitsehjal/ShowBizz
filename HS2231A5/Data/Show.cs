98f04f854b1834dd160ac2808f55a132b1b02ccb
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-08-03 15:09:09 -0400

    Create Design model fro 'Show' Entity

---
+++ HS2231A5/Data/Show.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HS2231A5.Data
+{
+    public class Show
+    {
+        public Show()
+        {
+            ReleaseDate = DateTime.Now;
+            Actors = new HashSet<Actor>();
+            Episodes = new HashSet<Episode>();
+        }
+        public int Id { get; set; }
+
+        // Name of the show; For example("WWE Smackdown")
+        [Required, StringLength(150)]
+        public string Name { get; set; }
+
+        // 'unassociated' genre name
+        [Required, StringLength(50)]
+        public string Genre { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        // ImageURL - TV series Cover art
+        [Required, StringLength(250)]
+        public string ImageUrl { get; set; }
+
+        // `Coordinator` - username(e.g. executive@example.com)
+        [Required, StringLength(250)]
+        public string Coordinator { get; set; }
+
+
+        // Navigation Properties
+        public ICollection<Actor> Actors { get; set; }
+
+        public ICollection<Episode> Episodes { get; set; }
+
+    }
+
+}
