fbacf20518cad3ba5900dfeffd9f59241476db4d
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-08-03 15:08:29 -0400

    Create Design model class for "Episode" Entity

---
+++ HS2231A5/Data/Episode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HS2231A5.Data
+{
+    public class Episode
+    {
+        public Episode()
+        {
+            AirDate = DateTime.Now;
+            SeasonNumber = 1;
+            EpisodeNumber = 1;
+        }
+        public int Id { get; set; }
+
+        // Name; For example: "Final Smackdown"
+        [Required, StringLength(150)]
+        public string Name { get; set; }
+
+        // SeasonNumber
+        public int SeasonNumber { get; set; }
+
+        // EpisodeNumber
+        public int EpisodeNumber { get; set; }
+
+        // Genre
+        [Required]
+        public string Genre { get; set; }
+
+        // AirDate
+        [Required]
+        public DateTime AirDate { get; set; }
+
+        // ImageURL - episode cover art
+        [Required, StringLength(250)]
+        public string ImageUrl { get; set; }
+
+        // Clerk - authenticated User who adds the new Episode
+        [Required, StringLength(250)]
+        public string Clerk { get; set; }
+
+        // ShowId
+        public int ShowId { get; set; }
+
+        // Navigation Properties
+        [Required]
+        public Show Show { get; set; }
+    }
+}
