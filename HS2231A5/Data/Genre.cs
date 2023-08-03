03caab3f5a9731ba2bb5104cabbac371ac1f72de
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-08-03 15:08:51 -0400

    Create Design model class for "Genre" Entity

---
+++ HS2231A5/Data/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HS2231A5.Data
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        // Name of the Genre; For example: "Comedy"
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+    }
+}
